0394ad83fc117930fa0929fe5c5354518f1ffdde
trentwiles <wilest7@gmail.com>
2023-10-28 13:27:59 -0700

    more tests

--- API/api-test.py
+++ API/api-test.py
@@ -1,22 +1,27 @@
 import requests
 import random
 
+endpoint = "localhost"
+
 def testLogin(user, password):
-    r = requests.post("http://localhost:5000/api/v1/login", data={"username": user, "password": password})
+    r = requests.post(f"http://{endpoint}:5000/api/v1/login", data={"username": user, "password": password})
     return r.json()["result"]["password"]
 
 print(testLogin("sj", "s")) # Expected to return False
 print(testLogin("james", "s")) # Expected to return False
 print(testLogin("james", "admin123")) # Expected to return True
 
-def testCreateAccount(user, password, realName, userType, profileImageURL, userRole):
-    r = requests.post("http://localhost:5000/api/v1/createAccount", data={"username": user, "password": password, "realName": realName, "userType": userType, "profileImageURL": profileImageURL})
+def testCreateAccount(user, password, realName, userType, profileImageURL):
+    r = requests.post(f"http://{endpoint}:5000/api/v1/createAccount", data={"username": user, "password": password, "realName": realName, "userType": userType, "profileImageURL": profileImageURL})
     return r.json()["result"]
 
-print(testCreateAccount("nick", "bob123")) # Expected to return "Username exists"
-print(testCreateAccount(f"demouser{str(random.randint(0,100000000))}", "mypassword123")) # Should always work
+print(testCreateAccount("trent", "demo123", "Trent Wiles", "student", "https://trentwil.es/a/FI3S64vsT4.png")) # Expected to return "Username exists"
+print(testCreateAccount("trent" + str(random.randint(0,1000000)), "demo123", "Trent Wiles", "student", "https://trentwil.es/a/FI3S64vsT4.png"))
 
 def testListUsers():
-    return requests.get("http://localhost:5000/api/v1/listAllUsers").json()
+    return requests.get(f"http://{endpoint}:5000/api/v1/listAllUsers").json()
 
 print(testListUsers())
+
+def testCreateRequest(username, title, description, tags):
+    return requests.post(f"http://{endpoint}:5000/api/v1/createNewRequest", data={"username": username, "title": title, description: "description", "tags": tags})
