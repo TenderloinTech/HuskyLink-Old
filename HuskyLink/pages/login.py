f6e6e654e2bde4c8133b3f6a468b133e9cb886e0
Anish Sahoo <anishsahoo2005@gmail.com>
2023-10-28 15:27:29 -0700

    added integration for profile

--- HuskyLink/pages/login.py
+++ HuskyLink/pages/login.py
@@ -10,21 +10,25 @@ def login() -> rx.Component:
     Returns:
         The UI for the dashboard page.
     """
-    # if State.loggedIn is True:
-    #     return rx.redirect("/dashboard")
+    if State.loggedIn is True:
+        print("reached state.loggedIn in login.py")
+        return rx.vstack(
+            rx.heading("You are Logged In")
+        )
+
     return rx.vstack(
-        rx.form(
-        rx.vstack(
-        rx.heading("Login Page", font_size="3em"),
-        rx.text("Please log in!"),
-        rx.hstack(
-            rx.text("Username:"),  # add text field for username
-            rx.input(id="username", placeholder="Username", size="lg"),
-        ),
-        rx.hstack(
-            rx.text("Password:"),  # add text field for password
-            rx.input(id="password", placeholder="Password", size="lg"),
-        ),
-        rx.button("Login", color="primary", size="lg", type_="submit")
-        ), on_submit=State.loginAuth, )
-    )
+    rx.form(
+    rx.vstack(
+    rx.heading("Login Page", font_size="3em"),
+    rx.text("Please log in!"),
+    rx.hstack(
+        rx.text("Username:"),  # add text field for username
+        rx.input(id="username", placeholder="Username", size="lg"),
+    ),
+    rx.hstack(
+        rx.text("Password:"),  # add text field for password
+        rx.input(id="password", placeholder="Password", size="lg"),
+    ),
+    rx.button("Login", color="primary", size="lg", type_="submit")
+    ), on_submit=State.loginAuth, )
+)
