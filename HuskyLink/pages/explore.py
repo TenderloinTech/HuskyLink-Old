8db857fb0dcef0688e2ba4cb67ea363c332cb0a4
trentwiles <wilest7@gmail.com>
2023-10-28 20:33:19 -0700

    testing table

--- HuskyLink/pages/explore.py
+++ HuskyLink/pages/explore.py
@@ -12,17 +12,19 @@ def explore() -> rx.Component:
     Returns:
         The UI for the explore page.
     """
+
+    rows_code = []
+    r = requests.get("https://api.tenderloin.tech/api/v1/listRequests").json()
+    for x in r.json():
+        tup = (x[0], 30, x[1], rx.button("View Details", color="#FF0000", variant='ghost'))
+        rows_code.append(tup)
     return rx.vstack(
         rx.heading("Explore", font_size="3em"),
         rx.text("This is where you will find people to link up with!"),
         rx.table_container(
             rx.table(
                 headers=["Name", "Age", "Location", "Button"],
-                rows=[
-                    ("John", 30, "New York", rx.button("View Details", color="#FF0000", variant='ghost')),
-                    ("Jane", 31, "San Francisco", rx.button("View Details", color="#FF0000",variant='ghost')),
-                    ("Joe", 32, "Los Angeles", rx.button("View Details", color="#FF0000", variant='ghost')),
-                ],
+                rows=rows_code,
                 variant="striped",
             )
         )
