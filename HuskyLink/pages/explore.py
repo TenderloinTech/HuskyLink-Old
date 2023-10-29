064ce0eabd76c3945be353e51df52652bd3ee3af
Anish Sahoo <anishsahoo2005@gmail.com>
2023-10-28 20:36:55 -0700

    working table demo

--- HuskyLink/pages/explore.py
+++ HuskyLink/pages/explore.py
@@ -15,7 +15,7 @@ def explore() -> rx.Component:
 
     rows_code = []
     r = requests.get("https://api.tenderloin.tech/api/v1/listRequests").json()
-    for x in r.json():
+    for x in r:
         tup = (x[0], 30, x[1], rx.button("View Details", color="#FF0000", variant='ghost'))
         rows_code.append(tup)
     return rx.vstack(
