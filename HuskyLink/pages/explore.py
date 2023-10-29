9dda957e5c6e73d25049a9dba364c32908b1c1ad
trentwiles <wilest7@gmail.com>
2023-10-28 19:53:36 -0700

    let's see if that works

--- HuskyLink/pages/explore.py
+++ HuskyLink/pages/explore.py
@@ -1,7 +1,16 @@
 """The explore page."""
 from HuskyLink.templates import template
+import requests
 
 import reflex as rx
+
+def createExplorePageTable():
+    html = "<table><th><tr>Title</tr><tr>Description</tr><tr>Created By</tr></th>"
+    r = requests.get("https://api.tenderloin.tech/api/v1/listRequests")
+    for x in r.json():
+        html += f"<th><td>{x[0]}</td><td>{x[1]}</td><td>{x[7]}</td>"
+    html += "</table>"
+    return html
 
 
 @template(route="/explore", title="Explore")
@@ -14,5 +23,6 @@ def explore() -> rx.Component:
     return rx.vstack(
         rx.heading("Explore", font_size="3em"),
         rx.text("This is where you will find people to link up with!"),
+        rx.html(createExplorePageTable())
         
     )
