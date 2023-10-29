63dc8cb1bf67b631899f67a46dfec737567a047b
trentwiles <wilest7@gmail.com>
2023-10-28 23:03:23 -0700

    code for anish to test

--- HuskyLink/pages/view.py
+++ HuskyLink/pages/view.py
@@ -4,23 +4,16 @@ import requests
 import reflex as rx
 
 
-@template(route="/view", title="View")
-def explore() -> rx.Component:
+@template(route="/view/<id>", title="View")
+def explore(id) -> rx.Component:
     """The view page.
 
     Returns:
         The UI for the view page.
     """
 
+    r = requests.get(f"https://api.tenderloin.tech/api/v1/getRequestByID/{id}")
+
     return rx.vstack(
-        rx.heading(", font_size="3em"),
-        rx.text("Welcome to Reflex!"),
-        rx.image(src=f"{State.profile_image}",    width="auto",
-    height="200px",
-    box_shadow="lg",),
-        rx.text(f"Name: {State.name}"),  # add text fields for first and last name
-        rx.text(f"Password: {len(str(State.password)) | 0} Characters"),  # add text field for password
-        rx.text(f"Role: {State.role}"),
-        rx.text(f"Is banned? {State.is_banned}"),
-        rx.button("Save Changes", color="primary", size="lg"),
+        rx.heading(f"{r.json()[0][0]}", font_size="3em")
     )
