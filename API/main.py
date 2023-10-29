814bc1951de2c543ca5b5397dd034adebca3cc02
trentwiles <wilest7@gmail.com>
2023-10-28 17:43:39 -0700

    stop listening everything

--- API/main.py
+++ API/main.py
@@ -184,4 +184,4 @@ def searchRequests():
         return Response(json.dumps(hits), content_type="application/json")
 # Run the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
