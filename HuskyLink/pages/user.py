074f2b5c2fec5b85a56e15523389bb54182be55b
trentwiles <wilest7@gmail.com>
2023-10-29 02:47:38 -0700

    user page for anish to test

--- HuskyLink/pages/user.py
+++ HuskyLink/pages/user.py
@@ -15,10 +15,9 @@ def view(name) -> rx.Component:
     r = requests.get(f"https://api.tenderloin.tech/api/v1/getUserInfo/{name}")
 
     return rx.vstack(
-        rx.heading(f"{r.json()[0][1]} ({r.json()[0][0]})", font_size="3em")
-        rx.link(href=f"mailto:{r.json()[0][7]}", rx.text(f"Email: {r.json()[0][7]}")),
+        rx.heading(f"{r.json()[0][1]} ({r.json()[0][0]})", font_size="3em"),
+        rx.link(rx.text(f"Email: {r.json()[0][7]}"), href=f"mailto:{r.json()[0][7]}"),
         rx.text(f"Role: {r.json()[0][3]}"),
-        rx.image(src=f"{r.json()[0][4]}",    width="auto",
-    height="200px")
+        rx.image(src=f"{r.json()[0][4]}", width="auto", height="200px")
 
     )
